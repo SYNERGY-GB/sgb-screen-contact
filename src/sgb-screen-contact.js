46ae5b24368cb96261117a56fed69bc38c91531c
Juan Garcia <juan.garcia@synergy-gb.com>
2015-05-14 08:45:28 -0500

    Developing and testing for TechDay App

--- src/sgb-screen-contact.js
+++ src/sgb-screen-contact.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('sgb-screen-contact', ['megazord'])
-  .controller('sgb-screen-contact-controller', ['$stateParams', '_screenParams', '$scope', function($stateParams, _screenParams, $scope){
+    .controller('sgb-screen-contact-controller', ['$stateParams', '_screenParams', '$scope', function($stateParams, _screenParams, $scope){
 
-      $scope.title = _screenParams.title || 'contact_info_title';
-      $scope.contactInfo = $stateParams.data;
+        $scope.title = _screenParams.title || 'contact_info_title';
+        $scope.contactInfo = $stateParams.data;
 
-      $scope.launchTwitter = function(){};
+        $scope.launchTwitter = function(){};
 
-      $scope.launchFacebook = function(){};
+        $scope.launchFacebook = function(){};
 
-      $scope.launchLinkedIn = function(){};
+        $scope.launchLinkedIn = function(){};
 
-      $scope.launchYouTube = function(){};
+        $scope.launchYouTube = function(){};
 
-      $scope.launchOther = function(){};
+        $scope.launchOther = function(){};
 
-  }]);
+    }]);
